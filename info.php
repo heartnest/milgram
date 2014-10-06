fda32996c329875f92fa91532e18dd90521136e4
heartnest <granwit@gmail.com>
2014-10-06 15:11:36 +0200

    info page added

---
+++ info.php
@@ -0,0 +1,124 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>General Information | Milgram Experiment Project | Disi Unibo</title>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<meta charset="utf-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name='Description' content='a web app for the test of milgram experiment' />
+  <meta name="author" content="liu tong">
+  <link rel="shortcut icon" href="favicon.ico">
+  
+  <script type='text/javascript' src='js/lib/jquery-1.7.2.min.js'></script>
+  <link type='text/css' href='css/bootstrap.css' rel='stylesheet' />
+  <script type='text/javascript' src='js/lib/bootstrap.min.js'></script>
+
+  <link href="css/style.css" rel="stylesheet">
+  <style>
+  li{
+  	list-style-type: none;
+  }
+  </style>
+</head>
+
+<body >
+ <div class="container">
+ 	<h1 class="text-center">Welcome to Milgram Reloaded!</h1>
+ 	<div>
+ 		<p class="text-center lead">
+Thank you for participating in the experiment.
+	</p>
+	<p>
+<label>Project title</label>: Milgram Reloaded!
+</p>
+
+<p>
+<label>Study team</label>:
+<ul>
+<li>Danilo Montessi, Professor, University of Bologna. Email: danilo.montesi@unibo.it</li>
+<li>Matteo Magnani, Associate Professor, Uppsala University. Email:matteo.magnani@it.uu.se</li>
+<li>Luca Rossi, Associate Professor, Uppsala University. Email:lucr@itu.dk</li>
+<li>Rajesh Sharma, Research Fellow, University of Bologna. Email: rajesh.sharma@unibo.it</li>
+<li>Tong Liu, Master student, University of Bologna. Email: tong.liu2@studio.unibo.it</li>
+</ul>
+</p>
+
+<p>
+<label>Funding provided by:</label>
+IMPACT Project, FIRB 2010-Ref:RBFR107725-001
+</p>
+
+<p>
+<h3>Introduction</h3>
+A lot of efforts have been made in the past to understand the process of navigation in a small world. We briefly describe the two important and related works. First is the Travers and Milgram experiment [1], which is a seminal work and secondly, a follow up work by Dodds et al. [3] before describing our approach in section 2.
+The classic 1969 field experiment by Travers and Milgram [1], was a huge step in the direction of understanding the navigation problem in the small world community. Arbitrarily (and as well as varying) individuals (from Nebraska and Boston) were asked to generate acquaintance chains for a single target person in Massachusetts, employing the small world method [2]. Only single medium of communication among acquaintances, the postcards were used to reach the target person.
+On similar lines to that of Travers and Milgram experiment, an experiment was conducted by Dodds et al. in 2003 [3]. Instead of postcards, an email tracking system was employed to understand the navigation process. This web-based experiment was different from the previous experiment [1] in two ways. Firstly, it was on a global scale (compared to just the experiment in [1], which was just conducted in America) with participants covering 13 countries. Secondly, multiple targets (18) were selected (compared to just the experiment in [1], which had just 1 target).
+The advancement of technology in the last decade has introduced various communication channels at the disposal of users. The presence of an individual at more than one network, assist in the formation of multilayer networks [4] so as to say.
+The main \textbf{goal} of this experiment is to understand the effect of various communication channels on the navigation process. One of the parameters to be observed is the average path length. In the classic Milgram experiment [1], it was observed that the average path length from the originator to the target is 6. It would be interesting to observe the effect of various communication channels on average path length in multilayer networks.
+</p>
+
+<p>
+
+	<hr />
+<label>Who can participate in the study</label><br/>
+All are welcome to participate in the study.
+</p>
+
+<p>
+<label>Potential benefits of the study</label><br/>
+Participating in the study might not benefit you directly, but your participation in the study will contribute to building knowledge surrounding social media use in education, and will lead to an understanding of successful strategies towards integrating social media in education, assessing social media with respect to teaching and learning, and contributing to ongoing discourse and research in the fields of e-learning, learning analytics, and social media.
+</p>
+
+<p>
+<label>Potential risks of the study</label><br/>
+The risks associated with this study are minimal, and there are no known risks for participating in this research.
+No personal identifying information will be required in the survey.
+</p>
+
+<p> 
+<label>How your information will be protected</label><br/>
+Your anonymity and confidentiality will be respected!
+</p>
+
+<p>
+The anonymity of participants will be achieved by not associating their names with data collected during the study. Participants will not be identified in any reports or publications. All information is kept confidential. That means it is accessible only by the investigators (i.e., password-protected). Data will only be shared with fellow researchers after applying privacy protection algorithms [5]. To put it in simple words, the readers will just see the numeric values for each of the publicly made entities. 
+<br/>
+This online survey is hosted by university of Bologna, Italy and as such is subject to that jurisdiction's laws. The websurvey company servers record incoming IP addresses of the computer that you use to access the survey but no connection is made between your data and your computer’s IP address. If you choose to participate in the survey, you understand that your responses to the survey questions will be stored in Italy. 
+
+</p>
+<p>
+<label>Contact for information about the study</label>
+<br/>
+If you have any questions or concerns about what we are asking of you, please contact:
+Rajesh Sharma
+connect@cs.unibo.it
+University of Bologna, Italy
+</p>
+<p>
+<label>Participant Consent</label>
+<br/>
+Taking part in this study is entirely up to you. You have the right to refuse to participate in this study. By participating in the survey you are providing your consent to use the data for purposes of research.
+
+Please indicate that you understand the information presented above, and consent to participate in this study.
+</p>
+<p>
+<label>References</label>
+<br/>
+<ul>
+<li>[1] Jeffrey Travers and Stanley Milgram, An experimental study of the Small World Problem, Psychology Today, 1967, Vol. 2, 60–67.</li>
+<li>[2] Stanley Milgram, "The Small World Problem", Psychology Today, 1967, Vol. 2, 60–67.</li>
+<li>[3] An Experimental Study of Search in Global Social Networks, Peter Sheridan Dodds , Roby Muhamad , Duncan J. Watts, Science, 2003, 301, page (827-829)</li>
+<li>[4] Mikko Kivela, Alexandre Arenas, Marc Barthelemy, James P. Gleeson, Yamir Moreno, and Mason A. Porter, “Multilayer Networks,” in arXiv preprint arXiv:1309.7233 (2013) arXiv:1309.7233.</li>
+<li>[5] Rui Chen, Noman Mohammed, Benjamin C. M. Fung, Bipin C. Desai, and Li Xiong. Publishing Set-Valued Data via Differential Privacy. PVLDB 4(11), 2011.</li>
+</ul>
+</p>
+ 	</div>
+</div>
+</body>
+</html>
