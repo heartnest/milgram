757b384c1805fd7c376f6e42600565bea9e12d2e
heartnest <granwit@gmail.com>
2015-10-21 23:30:31 +0200

    lan and trivals

--- lang/cn.php
+++ lang/cn.php
@@ -28,6 +28,46 @@ if ($targetid == '1') {
 }
 
 
+if ($targetid == '300') {
+	$targ_description = "Mostafa Salehi 在2012年从伊朗沙力夫理工大学读取的博士, 2013年在德黑兰大学当助理教授。2014年2月到9月，他在意大利博洛尼亚读的博士后。";
+	$targ_title = "Mostafa Salehi";
+}else if ($targetid == '311') {
+	$targ_description = "Payam Siyari 在伊朗Shahid Beheshti Univeristy大学读的计算机本科。2011年从沙力夫大学读取的研究生。2013年起，他在美国佐治亚理工学院攻读博士学位。 ";
+	$targ_title = "Payam Siyari";
+}else if ($targetid == '322') {
+	$targ_description = "Alessandro Rioli 2012年1月起读量子计算方向的计算机博士，他是物理学家，目前在一所理科高中教数学。他来自意大利博洛尼亚。";
+	$targ_title = "Alessandro Rioli";
+}else if ($targetid == '333') {
+	$targ_description = "Antony Karatzas 之前在英国Norwich生活了8年. 开始学习，之后在East Anglia大学的竞争研究中心工作。从2014年10月起，他移居 Leamington Spa 
+	并且在Warwick大学从事博士后研究。 他祖籍是希腊人，如今32岁了。他是足球爱好者，尤其喜欢 Liverpool FC。英国的朋友都管他叫'Tony'.";
+	$targ_title = "Antony Karatzas";
+}else if ($targetid == '344') {
+	$targ_description = "Amrith 从2008年8月起就在新加坡生活。开始是南洋理工(NTU)的博士生，之后创业，开设了一个帮助公司聘用数据师的网站。他目前30岁。家乡是印度Kerala。";
+	$targ_title = "Amrith";
+}else if ($targetid == '355') {
+	$targ_description = "Deepak Subramanian 是法国雷恩市(Rennes) CentraleSupelec的博士生. 他在新加坡生活过，祖籍是印度Chennai人";
+	$targ_title = "Deepak Subramanian";
+}
+else if ($targetid == '366') {
+	$targ_description = "Prithee Maga 是马来西亚理工大学年长的讲师。她从澳大利亚墨尔本RMIT大学毕业. 她是马来西亚Skudai, Johor的人. 她喜欢郊游，观光，野营. 
+	她的兴趣还有玩羽毛球，骑自行车。";
+	$targ_title = "Prithee";
+}
+else if ($targetid == '377') {
+	$targ_description = "Sandeep Ranjan 来自印度贾朗达尔(Jalandhar). 他是印度旁遮普科技大学(Punjab Technical University)的博士生。 他本科则是在DAV Institute of Engineering and Technology读的. 
+	之后在那里工作了7年。当前他在印度贾朗达尔的Lyallpur Khalsa College of Engineering大学计算机系工作。并且已经工作了2年。";
+	$targ_title = "Sandeep Ranjan";
+}
+else if ($targetid == '388') {
+	$targ_description = "Jiang Shan 在中国成都理工读本科的最后一年。在那边，他学习物联网(IoT). 他2014年曾经担任过计算机室的管理. 他的家乡是河北秦皇岛. 他是个高个子男生，思维活跃社交广。 ";
+	$targ_title = "Jiang Shan";
+}
+else if ($targetid == '399') {
+	$targ_description = "Jessica Liebig 是墨尔本RMIT大学的博士生. 她从德国不来梅（Bremen）出生，2007年到的澳大利亚. 
+	在2009年开始读本科生之前，她就开始周游澳大利亚周围的朋友都叫她Jessi 或者 Jess. 她的业余时间主要从事阅读，画画，慢跑 ";
+	$targ_title = "Jessica Liebig";
+}
+
 
 /***************  COMMON ******************/
 
